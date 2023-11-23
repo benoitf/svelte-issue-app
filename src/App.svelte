579f8e814663b9b5bc205c9781dbe1f417b1ac8a
Florent Benoit <fbenoit@redhat.com>
2023-11-23 15:03:53 +0100

    reproduce issue with vite-plugin-svelte

---
+++ src/App.svelte
@@ -0,0 +1,11 @@
+<script>
+  
+  import Fa from 'svelte-fa';
+  import { faCircleInfo} from '@fortawesome/free-solid-svg-icons';
+</script>
+
+<main>
+  FontAwesome icon: <Fa size="100" icon="{faCircleInfo}" />
+
+</main>
+
